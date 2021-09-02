b75eb748e2acdf0b0fe3ab5814a0000c1e89bb50
Alberto Zavala <jzavala@efim.com>
2021-09-02 10:33:07 -0700

    Se agregó lista de photocards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { ListOfCategories } from './components/ListOfCategories';
+import { ListOfPhotoCards } from './components/ListOfPhotoCards';
+
 import { GlobalStyle } from './GlobalStyles';
 
 export const App = () => (
   <>
-  <GlobalStyle/>
-  <ListOfCategories />
+    <GlobalStyle />
+    <ListOfCategories />
+    <ListOfPhotoCards />
   </>
 );
--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Category } from '../Category';
 
 import { List, Item } from './styles';
-import  * as db from '../../../api/db.json';
+import * as db from '../../../api/db.json';
 
 export const ListOfCategories = () => {
   return (
     <List>
       {
         db.categories.map((category) =>
-          <Item key={category.id}> <Category { ...category } /> </Item>
+          <Item key={category.id}> <Category {...category} /> </Item>
         )
       }
     </List>
