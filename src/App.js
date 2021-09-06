aace488bf2d93b88cd2621350ac53d6679c87f2d
Alberto Zavala <jzavala@efim.com>
2021-09-06 14:00:36 -0700

    Preparación de rutas para control de usuarios

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React from "react";
 
 import { GlobalStyle } from "./components/styles/GlobalStyles";
 import { Logo } from "./components/Logo";
+import { Router } from "@reach/router";
 
+import { NavBar } from "./components/NavBar";  
 import { Home } from "./pages/Home";
 import { Detail } from "./pages/Detail";
-import { Router } from "@reach/router";
-import { NavBar } from "./components/NavBar";  
+import { NotRegisteredUser } from "./pages/NotRegisteredUser";
+import { Favs } from "./pages/Favs";
+import { User } from "./pages/User";
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: false });
+}
 
 export const App = () => {
   return (
@@ -19,6 +26,24 @@ export const App = () => {
         <Home path="/pet/:categoryId" />
         <Detail path='/detail/:detailId' />
       </Router>
+
+      <UserLogged>
+        {
+          ({isAuth}) => 
+          isAuth
+          ? 
+          <Router>
+            <Favs path='/favs' />
+            <User path='/user' />
+          </Router>
+          :
+          <Router>
+            <NotRegisteredUser path='/favs'/>
+            <NotRegisteredUser path='/user'/>  
+          </Router>
+        } 
+      </UserLogged>
+
       <NavBar />
     </>
   );
