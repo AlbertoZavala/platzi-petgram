0b2244c126093c195c045b3d6c734705b698ba70
Jose Alberto Zavala Flores <jose.alberto.zavala.flores@gmail.com>
2021-09-05 19:44:10 -0700

    Se agregó hooks

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import { Category } from '../Category';
 
 import { List, Item } from './styles';
-import * as db from '../../../api/db.json';
+import db from './../../../api/db.json';
+
+function useCategoriesData () {
+  const [categories, setCategories] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(function () {
+    setLoading(true);
+    setCategories(db.categories);
+    setLoading(false);
+  }, []);
+
+  return { categories, loading };
+}
 
 export const ListOfCategories = () => {
-  return (
-    <List>
+  const { categories, loading } = useCategoriesData();
+
+  const [showFixed, setShowFixed] = useState(false);
+
+  useEffect(function () {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200;
+      showFixed !== newShowFixed && setShowFixed(newShowFixed);
+    };
+
+    document.addEventListener('scroll', onScroll);
+
+    return () => document.removeEventListener('scroll', onScroll);
+  }, [showFixed]);
+
+  const renderList = (fixed) => (
+    <List fixed={fixed}>
       {
-        db.categories.map((category) =>
-          <Item key={category.id}> <Category {...category} /> </Item>
-        )
+        loading
+          ? <Item key='loading' />
+          : categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
       }
     </List>
+  );
+
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   );
 };
