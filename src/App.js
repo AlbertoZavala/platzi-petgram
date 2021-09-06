e31cc878050051884f08ee819c00e55e8f458be4
Alberto Zavala <jzavala@efim.com>
2021-09-06 12:40:40 -0700

    Se agregó navbar

--- src/App.js
+++ src/App.js
@@ -1,33 +1,25 @@
-import React from 'react';
+import React from "react";
 
-import { GlobalStyle } from './components/styles/GlobalStyles';
-import { Logo } from './components/Logo';
-import { PhotoCardWithQuery } from './container/PhotoCardWithQuery';
+import { GlobalStyle } from "./components/styles/GlobalStyles";
+import { Logo } from "./components/Logo";
 
-import { Home } from './pages/Home';
-
-import { Router } from '@reach/router';
+import { Home } from "./pages/Home";
+import { Detail } from "./pages/Detail";
+import { Router } from "@reach/router";
+import { NavBar } from "./components/NavBar";  
 
 export const App = () => {
-  const urlParams = new window.URLSearchParams(window.location.search);
-  const detailId = urlParams.get('detail')
-
-  console.log(detailId);
-
-  return(
+  return (
     <>
-    <GlobalStyle />
-    <Logo />
-    {
-      detailId
-      ?
-        <PhotoCardWithQuery categoryId={detailId} />
-      :
+      <GlobalStyle />
+      <Logo />
+
       <Router>
-        <Home path='/'/>
-        <Home path='/pet/:categoryId'/>
+        <Home path="/" />
+        <Home path="/pet/:categoryId" />
+        <Detail path='/detail/:detailId' />
       </Router>
-    }    
-  </>
-  )
+      <NavBar />
+    </>
+  );
 };
