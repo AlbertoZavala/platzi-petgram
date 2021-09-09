465601795b7cb4b8c86eb628bcd244a0ba0088b1
Alberto Zavala <jzavala@efim.com>
2021-09-09 10:22:04 -0700

    Se agregó redireccionamiento

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import { GlobalStyle } from "./components/styles/GlobalStyles";
 import { Logo } from "./components/Logo";
-import { Router } from "@reach/router";
+import { Router, Redirect } from "@reach/router";
 
 import { NavBar } from "./components/NavBar";  
 import { Home } from "./pages/Home";
@@ -10,36 +10,29 @@ import { Detail } from "./pages/Detail";
 import { NotRegisteredUser } from "./pages/NotRegisteredUser";
 import { Favs } from "./pages/Favs";
 import { User } from "./pages/User";
-import Context from "./Context";
+import {Context} from "./Context";
+import { NotFound } from "./pages/NotFound";
 
 export const App = () => {
+  const { isAuth } = useContext(Context);  
+  
   return (
     <>
       <GlobalStyle />
       <Logo />
 
       <Router>
+        <NotFound default/>
         <Home path="/" />
         <Home path="/pet/:categoryId" />
         <Detail path='/detail/:detailId' />
+        { !isAuth && <NotRegisteredUser path='/login' /> }
+        { !isAuth && <Redirect from='/favs' to='/login'/> }
+        { !isAuth && <Redirect from='/user' to='/login'/> }
+        { isAuth && <Redirect from='/login' to='/ '  />}
+        <Favs path='/favs' />
+        <User path='/user' />
       </Router>
-
-      <Context.Consumer>
-        {
-          ({isAuth}) => 
-          isAuth
-          ? 
-          <Router>
-            <Favs path='/favs' />
-            <User path='/user' />
-          </Router>
-          :
-          <Router>
-            <NotRegisteredUser path='/favs'/>
-            <NotRegisteredUser path='/user'/>  
-          </Router>
-        } 
-      </Context.Consumer>
 
       <NavBar />
     </>
