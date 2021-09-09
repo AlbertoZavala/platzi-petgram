6deae9f4c326f69e169f264a64378a80c2d411d4
Jose Alberto Zavala Flores <jose.alberto.zavala.flores@gmail.com>
2021-09-08 20:13:35 -0700

    Refactor de NotRegisteredUser

--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -1,49 +1,55 @@
-import React from 'react'
-import Context from '../Context'
+import React, { useContext } from 'react'
+import { Context } from '../Context'
 import { UserForm } from '../components/UserForm'
 import { useRegisterUser } from '../hooks/useRegisterUser'
 import { useLoginUser } from '../hooks/useLoginUser'
 
 
 export const NotRegisteredUser = () => {
+    const { activateAuth } = useContext(Context)
+
     const { register, data : dataRegister, loading:  loadingRegister, error: errorRegister} = useRegisterUser('')    
     const { login, data: dataLogin, loading: loadingLogin, error: errorLogin} = useLoginUser('')    
     
     const errorMsgRegister = errorRegister && 'El usuario ya existe o hay algún problema';
     const errorMsgLogin = errorLogin && 'La contraseña no es correcta o el usuario no existe';
+
+    const registerAction = ({email, password}) => {
+        const input = { email, password }
+        const variables = { input }
+        register({ variables })
+        .then(({data}) => {
+            const { signup } = data
+          activateAuth(signup)
+        })
+    }
+
+    const loginAction = ({email, password}) => {
+        const input = { email, password }
+        const variables = { input }
+        login({ variables })
+        .then(({data}) => {
+            const { login } = data
+          activateAuth(login)
+        })
+    }
     
     return (
-      <Context.Consumer>
-          {
-              ({activateAuth}) => {
-                  const registerAction = ({email, password}) => {
-                      const input = { email, password }
-                      const variables = { input }
-                      register({ variables })
-                      .then(({data}) => {
-                          const { signup } = data
-                        activateAuth(signup)
-                      })
-                  }
-
-                  const loginAction = ({email, password}) => {
-                      const input = { email, password }
-                      const variables = { input }
-                      login({ variables })
-                      .then(({data}) => {
-                          const { login } = data
-                        activateAuth(login)
-                      })
-                  }
-                  
-                  return <>
-                      <UserForm disabled={loadingRegister} error={errorMsgRegister} onSubmit={registerAction} title='Registrarse'/>
-                      <UserForm disabled={loadingLogin} error={errorMsgLogin} onSubmit={loginAction} title='Iniciar Sesion'/>
-                  </>
-              }
-          }
-      </Context.Consumer>
-  )
+      <>
+        <UserForm
+          disabled={loadingRegister}
+          error={errorMsgRegister}
+          onSubmit={registerAction}
+          title="Registrarse"
+        />
+        <UserForm
+          disabled={loadingLogin}
+          error={errorMsgLogin}
+          onSubmit={loginAction}
+          title="Iniciar Sesion"
+        />
+      </>
+    );
 }
 
 
